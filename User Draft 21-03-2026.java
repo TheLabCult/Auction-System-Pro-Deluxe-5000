1f8a642d2501f688b6936a6fcf320110b669c8cf
TheLabCult <huy01759@gmail.com>
2026-03-21 07:16:40 +0700

    User Draft 21-03-2026

---
+++ User Draft 21-03-2026.java
@@ -0,0 +1,57 @@
+abstract class User {
+    protected String username;
+    protected String password;
+    protected String role;
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    abstract protected String getPassword();
+
+    public String getRole() {
+        return role;
+    }
+
+    public String toString() {
+        return "Username: " + username + ", Role: " + role;
+    }
+
+    public void changePassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    public void changeRole(String newRole) {
+        this.role = newRole;
+    }
+
+    public void changeUsername(String newUsername) {
+        this.username = newUsername;
+    }
+}
+class Bidder extends User {
+    public Bidder(String username, String password) {
+        super(username, password, "Bidder");
+    }
+
+    @Override
+    protected String getPassword() {
+        return this.password;
+    }
+}
+class Seller extends User {
+    public Seller(String username, String password) {
+        super(username, password, "Seller");
+    }
+
+    @Override
+    protected String getPassword() {
+        return this.password;
+    }
+}
