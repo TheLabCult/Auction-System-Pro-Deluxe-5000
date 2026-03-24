673e153be6079fbd748ee94c9a1db95d26f7f4cc
TheLabCult <huy01759@gmail.com>
2026-03-24 22:34:02 +0700

    Synced Art with other classes

--- src/com/auction/model/Art.java
+++ src/com/auction/model/Art.java
@@ -1,4 +1,20 @@
 package com.auction.model;
-public class Art {
-    
+public class Art extends Item {
+    private String artist;
+    private String ArtGenre;
+    public Art(String name, String description, double startingPrice, String artist, String ArtGenre) {
+        super(name, description, startingPrice);
+        artist = this.artist;
+        ArtGenre = this.ArtGenre;
+    }
+    public String getartist() {
+        return artist;
+    }
+    public String getArtGenre() {
+        return ArtGenre;
+    }
+    @Override
+    public String getDetails() {
+        return "[Art] " + getName() + " | Artist: " + artist + " ArtGenre: " + ArtGenre + " | Description: " + getDescription();
+    }
 }
