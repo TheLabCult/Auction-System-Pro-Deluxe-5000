aa6ba8b88c2608dfe94f43bfad3da689157a967a
linhoanguyen147 <linhoanguyen.147@gmail.com>
2026-03-25 13:16:23 +0700

    add model.readme

---
+++ src/README.md
@@ -0,0 +1,7 @@
+src/main/java/com/auction/
+ - model: Entity, User, Bidder, Item, Auction
+ - controller: các lớp xử lý luồng sự kiện từ giao diện (JavaFX Controller)
+ - service: logic (xử lý đặt giá đồng thời,...)
+ - dao: Data Access Object - code kết nối, truy cập Database
+ - network: client-server (socket / REST API)
+ - utils: tiện ích dùng chung (format thời gian, tiền tệ, ...)
--- src/com/auction/model/Seller.java
+++ src/com/auction/model/Seller.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 import java.util.List;
 public class Seller extends User {
     private String shopName;
+    private List<Item> myItems;
+    private double revenue;
     public Seller(String username, String password, String email, String fullName, String shopName) {
         super(username, password, email, fullName);
         this.shopName = shopName;
@@ -13,5 +15,8 @@ public class Seller extends User {
     }
     public String getShopName() {
         return shopName;
+    }
+    public void addRevenue(double amount) {
+        revenue += amount;
     }
 }
