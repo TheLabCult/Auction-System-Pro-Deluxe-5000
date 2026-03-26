0f244dfb20eeb285f432a9b8183fe5a3195c7a25
TheLabCult <huy01759@gmail.com>
2026-03-26 13:43:07 +0700

    Merged Bidder and Seller to User to allow participation in multiple auction sessions.

--- src/com/auction/model/BidTransaction.java
+++ src/com/auction/model/BidTransaction.java
@@ -1,15 +1,14 @@
 package com.auction.model;
 //luu tru thong tin cua 1 lan dat gia
-import java.time.LocalDateTime;
 public class BidTransaction extends Entity {
-    private Bidder bidder;
+    private User bidder;
     private double bidAmount;
-    public BidTransaction(Bidder bidder, double bidAmount) {
+    public BidTransaction(User bidder, double bidAmount) {
         super(); //lay id, createdAt (tgian dat gia)
         this.bidder = bidder;
         this.bidAmount = bidAmount;
     }
-    public Bidder getBidder() {
+    public User getBidder() {
         return bidder;
     }
     public double getBidAmount() {
--- src/com/auction/model/User.java
+++ src/com/auction/model/User.java
@@ -1,19 +1,29 @@
 package com.auction.model;
-public abstract class User extends Entity {
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User extends Entity {
     private String username;
     private String password;
     private String email;
     private String fullName;
     private boolean isActive; //state (admin can lock)
-    public User(String username, String password, String email, String fullName) {
+    private double balance;
+    private double frozen; //tiền bị giữ khi đấu giá
+    private String shopName;
+    private List<Item> myItems; // các sản phẩm
+    public User(String username, String password, String email, String fullName, String shopName) {
         super();
         this.username = username;
         this.password = password;
         this.email = email;
         this.fullName = fullName;
         this.isActive = true;
+        this.shopName = shopName;
+        this.myItems = new ArrayList<>();
     }
-    public abstract void displayRoleInfo();
+    // public abstract void displayRoleInfo();
     //getter
     public String getUsername() {
         return this.username;
@@ -24,6 +34,15 @@ public abstract class User extends Entity {
     public String getEmail() {
         return this.email;
     }
+    public double getBalance() {
+        return this.balance;
+    }
+    public String getShopName() {
+        return shopName;
+    }
+    public List<Item> getMyItems() { //get list of items for displaying (GUI)
+        return myItems;
+    }
     //setter
     public void setFullName(String fullName) {
         this.fullName = fullName;
@@ -33,6 +52,9 @@ public abstract class User extends Entity {
     }
     public boolean isActive() {
         return isActive;
+    }
+    public void addBalance(double amount) {
+        balance += amount;
     }
     public void setActive(boolean state) {
         isActive = state;
@@ -46,6 +68,37 @@ public abstract class User extends Entity {
     }
     public boolean verifyPassword(String password) {
         return this.password.equals(password);
+    }
+    public void addItem(Item item) {
+        myItems.add(item);
+        System.out.println("Added " + item.getName());
+    }
+    public boolean removeItem(Item item) {
+        if (myItems.remove(item)) {
+            System.out.println("Removed " + item.getName());
+            return true;
+        }
+        return false;
+    }
+    public void addRevenue(double amount) {
+        balance += amount;
+        System.out.println("Shop " + shopName + " received revenue of " + amount);
+    }
+    //khi đấu giá, trừ balance, cộng frozen
+    public boolean freezeMoney(double amount) {
+        if (balance < amount) return false;
+        balance -= amount;
+        frozen += amount;
+        return true;
+    }
+    //khi có người thắng với bid cao hơn -> refund
+    public void unfreezeMoney(double amount) {
+        frozen -= amount;
+        balance += amount;
+    }
+    //khi phiên kết thúc, thắng
+    public void payForWonAuction(double amount) {
+        frozen -= amount;
     }
 //    public void setPassword(String password) {
 //        this.password = password;
