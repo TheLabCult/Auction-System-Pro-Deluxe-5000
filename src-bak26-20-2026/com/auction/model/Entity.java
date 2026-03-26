ccac958488f75c0442228713d2275ab5c2af64d8
TheLabCult <huy01759@gmail.com>
2026-03-26 14:07:42 +0700

    Pushed backup to cloud

---
+++ src-bak26-20-2026/com/auction/model/Entity.java
@@ -0,0 +1,18 @@
+package com.auction.model;
+import java.time.LocalDateTime;
+import java.util.UUID;
+public abstract class Entity {
+    private final String id;
+    private final LocalDateTime createdAt;
+    public Entity() {
+        this.id = UUID.randomUUID().toString();
+        this.createdAt = LocalDateTime.now();
+    }
+    //getter
+    public String getId() {
+        return this.id;
+    }
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+}
