a8fb6186775e4911c5b59060515efaba807b67d0
LeAnhQuang_25021951 <le8940741@gmail.com>
2026-03-28 10:52:15 +0700

    small fixes
    tiếp tục chuyển các bidder, seller từ các bộ phận khác sang StandardUser

--- app/src/com/auction/model/Auction.java
+++ app/src/com/auction/model/Auction.java
@@ -6,7 +6,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class Auction extends Entity {
     //định danh, liên kết
     private Item item;
-    private Seller seller;
+    private StandardUser seller;
     //thời gian, trạng thái
     private LocalDateTime startTime;
     private LocalDateTime endTime;
@@ -16,7 +16,7 @@ public class Auction extends Entity {
     //CopyOnWriteArrayList đảm bảo thread-sàe khi có nhiều bid cùng lúc
     private final List<BidTransaction> bidHistory; //lich su dat gia an toan
 
-    public Auction(Item item, Seller seller, LocalDateTime startTime, LocalDateTime endTime) {
+    public Auction(Item item, StandardUser seller, LocalDateTime startTime, LocalDateTime endTime) {
         super();
         if (item.getStatus() != ItemStatus.IN_AUCTION) {
             throw new IllegalArgumentException("Item is in another auction or sold");
@@ -30,7 +30,7 @@ public class Auction extends Entity {
         this.bidHistory = new CopyOnWriteArrayList<>();
     }
     //đặt giá cốt lõi (thread-safe)
-    public synchronized boolean placeBid(Bidder bidder, double amount) {
+    public synchronized boolean placeBid(StandardUser bidder, double amount) {
         //kiem tra ngoai le (phien da dong hay chua?)
         if (this.status != AuctionStatus.RUNNING) {
             System.out.println("Refuse: Inactive Auction");
@@ -42,16 +42,31 @@ public class Auction extends Entity {
             System.out.println("Refuse: Auction amount must be greater than current price: " + curPrice);
             return false;
         }
-        //logic đóng băng tiền người đặt
-        if (!bidder.freezeMoney(amount)) {
-            System.out.println("Refuse: Bidder " + bidder.getUsername() + " does not have enough balance");
-        }
-        //logic hoan lai tien (unfrozen) cho nguoi top 1 hien tai
-        if (highestBid != null) {
-            Bidder prevLeader = highestBid.getBidder();
-            prevLeader.unfreezeMoney(highestBid.getBidAmount());
-            System.out.println("Refund " + highestBid.getBidAmount() + " for " + prevLeader.getUsername());
-        }
+
+
+
+
+//        //logic đóng băng tiền người đặt
+//        if (!bidder.freezeMoney(amount)) {
+//            System.out.println("Refuse: Bidder " + bidder.getUsername() + " does not have enough balance");
+//        }
+//        //logic hoan lai tien (unfrozen) cho nguoi top 1 hien tai
+//        if (highestBid != null) {
+//            Bidder prevLeader = highestBid.getBidder();
+//            prevLeader.unfreezeMoney(highestBid.getBidAmount());
+//            System.out.println("Refund " + highestBid.getBidAmount() + " for " + prevLeader.getUsername());
+//        }
+
+        /* Ở trong eBay aunction, người đấu giá thắng rồi mới trả tiền
+        * không phải là đặt bid là phải trả tiền luôn
+        * balance - số dư trên eBay không phản ánh đúng khả nănng tài chính của họ
+        * vì giống như ví Shoppee, cần mua thì người ta mới chuyênr từ tài khoản ngân hàng vào thôi
+        * nếu bắt buộc phải có một khoản frozen thì rất bất tiện
+        * đặt mua nhiều bid khác nhau lại chuyển phải thêm một khoản vào ví
+        * tăng bid lại phải thêm khoản nữa, rất nhiều thao tác tốn thời gian*/
+
+
+
         //update status for next leader
         BidTransaction newBid = new BidTransaction(bidder, amount);
         this.highestBid = newBid;
--- app/src/com/auction/service/UserManager.java
+++ app/src/com/auction/service/UserManager.java
@@ -1,7 +1,7 @@
 package com.auction.service;
 
-import com.auction.model.Bidder;
-import com.auction.model.Seller;
+//import com.auction.model.Bidder;
+import com.auction.model.StandardUser;
 import com.auction.model.User;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,30 +9,37 @@ public class UserManager {
     private Map<String, User> userDatabase; //key = username, value = User
     //singleton pattern: only 1 usermanager
     private static UserManager instance;
+
     private UserManager(){
         userDatabase = new HashMap<>();
     }
+
     public static UserManager getInstance(){
         if(instance == null){
             instance = new UserManager();
         }
         return instance;
     }
+
     //REGISTRATION
-    public Bidder registerBidder(String username, String password, String personalID, String email, String fullName){
+    public StandardUser registerStandardUser(String username, String password, String email, String fullName, double balance){
         if (userDatabase.containsKey(username)) throw new IllegalArgumentException("Username is already in use");
-        Bidder bidder = new Bidder(username, password, personalID, email, fullName, 0.0);
-        userDatabase.put(username, bidder);
+        StandardUser NewUser = new StandardUser(username, password, email, fullName, 0.0);
+        userDatabase.put(username, NewUser);
         System.out.println("Successfully registered Bidder: " + username);
-        return bidder;
+        return NewUser;
     }
-    public Seller registerSeller(String username, String password, String personalID, String email, String fullName, String shopName){
-        if  (userDatabase.containsKey(username)) throw new IllegalArgumentException("Username is already in use");
-        Seller seller = new Seller(username, password, personalID, email, fullName, shopName);
-        userDatabase.put(username, seller);
-        System.out.println("Successfully registered Seller: " + username);
-        return seller;
-    }
+
+
+//    public Seller registerSeller(String username, String password, String personalID, String email, String fullName, String shopName){
+//        if  (userDatabase.containsKey(username)) throw new IllegalArgumentException("Username is already in use");
+//        Seller seller = new Seller(username, password, personalID, email, fullName, shopName);
+//        userDatabase.put(username, seller);
+//        System.out.println("Successfully registered Seller: " + username);
+//        return seller;
+//    }
+
+
     //LOGIN
     public User login(String username, String password){
         User user = userDatabase.get(username);
