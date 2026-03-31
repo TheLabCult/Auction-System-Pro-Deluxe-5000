d5e381d90c401fb42629a54f41a8ccfd1dcb337e
TheLabCult <huy01759@gmail.com>
2026-03-31 10:05:38 +0700

    Added return to login button for register system

--- app/src/main/java/com/auction_pro_5000/controller/RegisterController.java
+++ app/src/main/java/com/auction_pro_5000/controller/RegisterController.java
@@ -1,12 +1,15 @@
 package com.auction_pro_5000.controller;
 
 import com.auction.service.UserManager;
+import com.auction_pro_5000.NavigationUtils;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class RegisterController {
 
@@ -15,6 +18,8 @@ public class RegisterController {
     @FXML private TextField txtFullName;
     @FXML private TextField txtpersonalID;
     @FXML private TextField txtemail;
+
+    @FXML private Button registerBtn;
 
     @FXML
     public void handleRegister() {
@@ -44,6 +49,15 @@ public class RegisterController {
         } catch (Exception e) {
             showAlert(AlertType.ERROR, "Thất bại", "Không thể đăng ký: " + e.getMessage());
         }
+
+        // Bây giờ loginBtn sẽ không còn bị null nữa
+        if (registerBtn != null && registerBtn.getScene() != null) {
+            // Lấy Stage hiện tại từ nút bấm
+            Stage stage = (Stage) registerBtn.getScene().getWindow();
+                        
+            // Chuyển sang file Login.fxml
+            NavigationUtils.navigate(stage, "Login.fxml", "Hệ thống đăng nhập");
+        }
     }
 
     private void showAlert(AlertType type, String title, String content) {
@@ -60,5 +74,12 @@ public class RegisterController {
         txtpersonalID.clear();
         txtemail.clear();
         txtFullName.clear();
+    }
+
+    @FXML
+    private void handleShowLogin() {
+        // Lấy Stage hiện tại và chuyển sang màn hình Login.fxml
+        Stage stage = (Stage) registerBtn.getScene().getWindow();
+        NavigationUtils.navigate(stage, "Login.fxml", "Đăng nhập tài khoản");
     }
 }
