38d09ff010d76871a58ee43a1b473c7b0cbcfe65
TheLabCult <huy01759@gmail.com>
2026-04-07 17:00:32 +0700

    Added file skeleton

--- app/src/main/java/com/auction/model/ItemType.java
+++
@@ -1,7 +0,0 @@
-package com.auction.model;
-
-public enum ItemType {
-    ELECTRONICS,
-    ART,
-    VEHICLE
-}
---
+++ auction system/server/src/main/java/com/auction/domain/entity/Entity.java
@@ -0,0 +1,4 @@
+abstract public class Entity {
+    private String id;
+    private String name;
+}
