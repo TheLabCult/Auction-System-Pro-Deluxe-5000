dea8d67da65bcee4901a7e16b1fe43971984f5a2
TheLabCult <huy01759@gmail.com>
2026-04-19 16:39:01 +0700

    Client JavaFX test

---
+++ src/main/java/com/auction/client/AuctionApp.java
@@ -0,0 +1,22 @@
+package com.auction.client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AuctionApp extends Application {
+    @Override
+    public void init() throws Exception {
+        System.out.println("Before start");
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Hello World!");
+        stage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("After stop");
+    }
+}
---
+++ src/main/java/com/auction/client/Main.java
@@ -0,0 +1,9 @@
+package com.auction.client;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(AuctionApp.class, args);
+    }
+}
